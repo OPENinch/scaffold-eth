7a51c0c9cb11080ca4256f1c83d234a2495c9deb
hiiamu <12mcomco@gmail.com>
2021-08-26 15:03:59 -0500

    improve tests

--- packages/hardhat/test/OneSplitTest.js
+++ packages/hardhat/test/OneSplitTest.js
@@ -1,6 +1,7 @@
 const { use, expect } = require("chai");
 const { solidity } = require("ethereum-waffle");
 const { ethers } = require("hardhat");
+const { BN, expectRevert } = require('@openzeppelin/test-helpers');
 
 use(solidity);
 
@@ -50,22 +51,25 @@ FLAG_ENABLE_REFERRAL_GAS_SPONSORSHIP = 0x80000000000000; // Turned off by defaul
 FLAG_ENABLE_CHI_BURN_BY_ORIGIN = 0x4000000000000000;
 
 FLAG_DISABLE_ALL = 0x1F2800000000C
+FLAG_ANY = 0x0;
 
-const eth       = ['0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE',      'ETH'];
-const weth      = ['0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',     'WETH'];
-const chai      = ['0x06AF07097C9Eeb7fD685c692751D5C66dB49c215',     'CHAI'];
-const dai       = ['0x6B175474E89094C44Da98b954EedeAC495271d0F',      'DAI'];
-const usdc      = ['0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',     'USDC'];
-const usdt      = ['0xdAC17F958D2ee523a2206206994597C13D831ec7',     'USDT'];
-const tusd      = ['0x0000000000085d4780B73119b644AE5ecd22b376',     'TUSD'];
-const busd      = ['0x4fabb145d64652a948d72533023f6e7a623c7c53',     'BUSD'];
-const susd      = ['0x57Ab1ec28D129707052df4dF418D58a2D46d5f51',     'SUSD'];
-const pax       = ['0x8E870D67F660D95d5be530380D0eC0bd388289E1',      'PAX'];
-const renbtc    = ['0xEB4C2781e4ebA804CE9a9803C67d0893436bB27D',   'RENBTC'];
-const wbtc      = ['0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599',     'WBTC'];
-const tbtc      = ['0x1bBE271d15Bb64dF0bc6CD28Df9Ff322F2eBD847',     'TBTC'];
-const hbtc      = ['0x0316EB71485b0Ab14103307bf65a021042c6d380',     'HBTC'];
-const sbtc      = ['0xfE18be6b3Bd88A2D2A7f928d00292E7a9963CfC6',     'SBTC'];
+const eth       = ['0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE',   'ETH',    1e18, '390000000'];
+const weth      = ['0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',   'WETH',   1e18, '390000000']; // passes eth -> weth on any
+const chai      = ['0x06AF07097C9Eeb7fD685c692751D5C66dB49c215',   'CHAI',   1e18, '390000000']; // fails eth  -> chai on any
+const dai       = ['0x6B175474E89094C44Da98b954EedeAC495271d0F',   'DAI',    1e18, '390000000']; // fails eth  -> dai on any
+const usdc      = ['0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',   'USDC',   1e6,  '390000000']; // fails eth  -> usdc on any
+const usdt      = ['0xdAC17F958D2ee523a2206206994597C13D831ec7',   'USDT',   1e6,  '390000000']; // passes eth -> usdt on any
+const tusd      = ['0x0000000000085d4780B73119b644AE5ecd22b376',   'TUSD',   1e18, '390000000']; // passes eth -> tusd on any
+const busd      = ['0x4fabb145d64652a948d72533023f6e7a623c7c53',   'BUSD',   1e18, '390000000']; // passes eth -> busd on any
+const susd      = ['0x57Ab1ec28D129707052df4dF418D58a2D46d5f51',   'SUSD',   1e18, '390000000']; // passes eth -> susd on any
+const pax       = ['0x8E870D67F660D95d5be530380D0eC0bd388289E1',   'PAX',    1e18, '390000000']; // passes eth -> pax on any
+const renbtc    = ['0xEB4C2781e4ebA804CE9a9803C67d0893436bB27D',   'RENBTC', 1e8,  '600000'];    // passes eth -> renbtc on any
+const wbtc      = ['0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599',   'WBTC',   1e8,  '600000'];    // passes eth -> wbtc on any
+const tbtc      = ['0x1bBE271d15Bb64dF0bc6CD28Df9Ff322F2eBD847',   'TBTC',   1e18, '600000'];    // fails eth -> tbtc on any
+const hbtc      = ['0x0316EB71485b0Ab14103307bf65a021042c6d380',   'HBTC',   1e18, '390000000']; // passes eth -> hbtc on any
+const sbtc      = ['0xfE18be6b3Bd88A2D2A7f928d00292E7a9963CfC6',   'SBTC',   1e18, '390000000']; // passes eth -> sbtc on any
+
+const list = [eth, weth, usdt, tusd, busd, susd, pax, renbtc, wbtc, hbtc, sbtc];
 
 describe("OneSplit test", function () {
     this.timeout(200000);
@@ -84,24 +88,30 @@ describe("OneSplit test", function () {
         OneSplitWrap = await OneSplitWrapDeployment.deploy(OneSplitViewWrap.address, OneSplit.address);
     });
 
+    
     from = eth;
-    to = busd;
-    decimal = 1e18
 
-    it(('should work with Curve ' + from[1] + ' => ' + to[1]).toString(), async function () {
-        const res = await OneSplitWrap.getExpectedReturn(
-            from[0], // From token
-            to[0], // Dest token
-            '1000000000000000000', // 1.0  // amount of from token
-            10, // parts, higher = more granular, but effects gas usage (probably exponentially)
-            //DISABLE_ALL + (CURVE_ALL), // flag (enable only curve)
-            FLAG_DISABLE_ALL - FLAG_DISABLE_CURVE_ALL,
-        );
+    for (var coin = 0; coin < list.length; coin++) {
+        if (list[coin] != from) {
+            to = list[coin];
+            decimal = to[2];
 
-        console.log('Swap: 1', from[1]);
-        console.log('returnAmount:', res.returnAmount.toString() / decimal, to[1]);
-        // console.log('distribution:', res.distribution.map(a => a.toString()));
-        // console.log('raw:', res.returnAmount.toString());
-        expect(res.returnAmount).to.be.bignumber.above('390000000');
-    });
+            it(('should work with ANY ' + from[1] + ' => ' + to[1]).toString(), async function () {
+                const res = await OneSplitWrap.getExpectedReturn(
+                    from[0], // From token
+                    to[0], // Dest token
+                    '1000000000000000000', // 1.0  // amount of from token
+                    10, // parts, higher = more granular, but effects gas usage (probably exponentially)
+                    //DISABLE_ALL + (CURVE_ALL), // flag (enable only curve)
+                    FLAG_ANY,
+                );
+        
+                console.log('Swap: 1', from[1]);
+                console.log('returnAmount:', res.returnAmount.toString() / decimal, to[1]);
+                // console.log('distribution:', res.distribution.map(a => a.toString()));
+                // console.log('raw:', res.returnAmount.toString());
+                expect(res.returnAmount).to.be.bignumber.above(to[3]);
+            });
+        }
+    }
 });
